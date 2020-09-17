10eaa7982d8b165f3e0891e2b0356e79c50268cb
111222333 <111222333>
2020-09-18 04:36:36 +0530

    Create java class body

--- CscExample.java
+++ CscExample.java
@@ -4,5 +4,42 @@ public class CscExample
 	public String category;
 	public String  text;
 	
+	public void  setQuectionNo()
+	{
+		
+	}
+	public void  setCategory()
+	{
+		
+	} 
+	public void setText()
+	{
+		
+	}
+	public int getQuectionNo()
+	{
+		
+	}
+	public  String getCategory()
+	{
+		
+	}
+	public String getText()
+	{
+		
+	}
 	
+ 
+	 public void addQuectionNo(CscExample qu)
+	 {
+		 
+	 }
+	 public void removeQuection(int quId)
+	 {
+		 
+	 }
+	 public void editQuction(int quId)
+	 {
+		 
+	 }
 }
