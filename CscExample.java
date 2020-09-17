c465536683e0aa542ec4af96aaa7c62866b51c40
111222333 <111222333>
2020-09-18 04:20:19 +0530

    add get and set method

---
+++ CscExample.java
@@ -0,0 +1,24 @@
+public void  setQuectionNo()
+{
+	
+}
+public void  setCategory()
+{
+	
+} 
+public void setText()
+{
+	
+}
+public int getQuectionNo()
+{
+	
+}
+public  String getCategory()
+{
+	
+}
+public String getText()
+{
+	
+}
