6a4456ca3e83805a738e2c7c016b71a051a3def0
111222333 <111222333>
2020-09-18 04:42:14 +0530

    Create example java file and add body

---
+++ Example.java
@@ -0,0 +1,45 @@
+public class CscExample
+{
+	public int questionNo;
+	public String category;
+	public String  text;
+	
+	public void  setQuectionNo()
+	{
+		
+	}
+	public void  setCategory()
+	{
+		
+	} 
+	public void setText()
+	{
+		
+	}
+	public int getQuectionNo()
+	{
+		
+	}
+	public  String getCategory()
+	{
+		
+	}
+	public String getText()
+	{
+		
+	}
+	
+ 
+	 public void addQuectionNo(CscExample qu)
+	 {
+		 
+	 }
+	 public void removeQuection(int quId)
+	 {
+		 
+	 }
+	 public void editQuction(int quId)
+	 {
+		 
+	 }
+}
