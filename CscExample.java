bbb2d6393f2f13270367a70c6f6d948f09a0c287
111222333 <111222333>
2020-09-18 05:37:00 +0530

    edit cscexample.java

--- CscExample.java
+++ CscExample.java
@@ -1,16 +1,4 @@
 
- public void addQuectionNo(CscExample qu)
- {
-	 
- }
- public void removeQuection(int quId)
- {
-	 
- }
- public void editQuction(int quId)
- {
-	 
- }
 
 public class CscExample
 {
@@ -20,26 +8,27 @@ public class CscExample
 	
 	public void  setQuectionNo()
 	{
-		
+            this.quectionNo=QuectionNo;		
 	}
 	public void  setCategory()
 	{
-		
+		this.category=category;
 	} 
 	public void setText()
 	{
-		
+		this.text=text;
 	}
 	public int getQuectionNo()
 	{
-		
+		return quectionNo;
 	}
 	public  String getCategory()
 	{
-		
+		return category;
 	}
 	public String getText()
 	{
+		return text;
 		
 	}
 	
