ca166ef1b7d1bfcfc0ed62146804b0a48459b0d3
tharindu45 <prasadtharindu816@gmail.com>
2020-11-29 22:13:11 -0800

    create home page

---
+++ home.php
@@ -0,0 +1,58 @@
+<html>
+<head>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+	<nav class="navbar-default nav-top">
+	  <div class="container-fluid">
+		<ul class="nav navbar-top" >
+		  <li><a href="#">Nav-A-1</a>
+
+		  </li>
+		  <li><a href="#">Nav-A-2</a>
+
+		  </li>
+		  <li><a href="#">.....</a>
+
+		  </li>
+		</ul>
+	  </div>
+	</nav>
+	<nav class="navbar navbar-default">
+	  <div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+		  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> <span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+
+		  </button>
+		  <a class="navbar-brand" href="#">
+			<img src="http://placehold.it/100x50/f00/f00">
+		  </a>
+
+		</div>
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		  <ul class="nav navbar-nav " style="margin-left:150px;">
+			<li class="active"><a href="#">Nav-B-1<span class="sr-only">(current)</span></a>
+
+			</li>
+			<li><a href="#">Nav-B-2</a>
+
+			</li>
+			<li><a href="#">Nav-B-3</a>
+
+			</li>
+			<button type="button" class="btn btn-default" aria-label="Left Align">
+               <span class="glyphicon glyphicon-align-left" aria-hidden="true"></span>
+          </button>
+		  </ul>
+		</div>
+	  </div>
+	</nav>
+</body>
+</html>
